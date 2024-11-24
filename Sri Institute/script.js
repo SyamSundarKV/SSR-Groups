cd17ba996ca7a94c56fc50981aa6b7c97d28bb81
SyamSundarKV <kvssr2001@gmail.com>
2024-11-24 23:19:26 +0530

    major update for all folders

--- Sri Institute/script.js
+++ Sri Institute/script.js
@@ -61,3 +61,20 @@ const activeElem = function () {
 }
 
 addEventOnElem(window, "scroll", activeElem);
+
+
+
+window.onload = () => {
+  const popup_btns = document.querySelectorAll('.popup-button');
+
+  popup_btns.forEach(button => {
+      button.addEventListener('click', e => {
+          const target = e.target.dataset.target;
+
+          const popup_el = document.querySelector(target);
+          if (popup_el != null) {
+              popup_el.classList.toggle('is-active');
+          }
+      });
+  });
+}
--- blog/assets/js/script.js
+++ blog/assets/js/script.js
@@ -57,3 +57,18 @@ const toggleSearchBar = function () {
 }
 
 addEventOnElem(searchTogglers, "click", toggleSearchBar);
+
+window.onload = () => {
+  const popup_btns = document.querySelectorAll('.popup-button');
+
+  popup_btns.forEach(button => {
+      button.addEventListener('click', e => {
+          const target = e.target.dataset.target;
+
+          const popup_el = document.querySelector(target);
+          if (popup_el != null) {
+              popup_el.classList.toggle('is-active');
+          }
+      });
+  });
+}
