ef22bf80298cb867fa0c30d389772da4e5882503
SyamSundarKV <kvssr2001@gmail.com>
2024-11-24 22:31:29 +0530

    terms update

--- js/main.js
+++ js/main.js
@@ -112,3 +112,20 @@
     
 })(jQuery);
 
+
+
+
+window.onload = () => {
+    const popup_btns = document.querySelectorAll('.popup-button');
+
+    popup_btns.forEach(button => {
+        button.addEventListener('click', e => {
+            const target = e.target.dataset.target;
+
+            const popup_el = document.querySelector(target);
+            if (popup_el != null) {
+                popup_el.classList.toggle('is-active');
+            }
+        });
+    });
+}
